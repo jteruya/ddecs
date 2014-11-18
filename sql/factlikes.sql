762cb19c74ef054628efe95893a5cc639e8611d2
kchiou <kchiou@doubledutch.me>
2014-11-18 15:46:39 -0800

    Added Comments

--- sql/factlikes.sql
+++ sql/factlikes.sql
@@ -1,6 +1,15 @@
 IF OBJECT_ID('ReportingDB.dbo.FactLikes','U') IS NOT NULL
   DROP TABLE ReportingDB.dbo.FactLikes
 
+--===================================================================================================
+-- Source on User Likes Fact table and enforces that each like is tied to an identified user. 
+-- * Upstream dependency on DimUsers.
+--
+-- Minor transformations:
+-- 1. Translation of numeric code to string value. 
+-- 2. Flag inidcator that related post has image. 
+--===================================================================================================
+
 SELECT DISTINCT S.Created Timestamp, P.ApplicationId, GlobalUserId, S.UserId,
 CASE
   WHEN ListTypeId = 0 THEN 'Unspecified'
