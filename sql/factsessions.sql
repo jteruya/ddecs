c5747b5d5267794906d19c686047bf4ad212ae6a
kchiou <kchiou@doubledutch.me>
2014-11-18 14:13:03 -0800

    Added Comments

--- sql/factsessions.sql
+++ sql/factsessions.sql
@@ -1,6 +1,16 @@
 IF OBJECT_ID('ReportingDB.dbo.FactSessions') IS NOT NULL
   DROP TABLE ReportingDB.dbo.FactSessions
 
+--===================================================================================================
+-- Source on Sessions Fact table and enforces that each session is tied to an identified user. 
+-- * Upstream dependency on DimUsers.
+--
+-- Minor transformations:
+-- 1. Binary Version identified through string parsing. 
+-- 2. Device value translated from numeric code to string. 
+-- 3. Device Type/OS translated from numeric code to string.
+--===================================================================================================
+
 SELECT DISTINCT StartDate Timestamp, S.ApplicationId, GlobalUserId, S.UserId,
 CASE WHEN BinaryVersion IS NULL THEN 'v???' WHEN LEFT(BinaryVersion,1) = 'v' THEN 'v'+RIGHT(LEFT(BinaryVersion,4),3) ELSE 'v'+LEFT(BinaryVersion,3) END BinaryVersion,
 CASE
