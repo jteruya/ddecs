d532deb7dc39d8201273cbb75301678852a62ef1
kchiou <kchiou@doubledutch.me>
2014-11-18 15:52:37 -0800

    Added Comments

--- sql/factsurveys.sql
+++ sql/factsurveys.sql
@@ -1,6 +1,15 @@
 IF OBJECT_ID('ReportingDB.dbo.FactSurveys','U') IS NOT NULL
   DROP TABLE ReportingDB.dbo.FactSurveys
 
+--===================================================================================================
+-- Source on Survey Responses Fact table and enforces that each rating is tied to an identified user. 
+-- * Upstream dependency on DimUsers.
+--
+-- Minor transformations:
+-- 1. Metadata tied to Survey Responses (Question, Survey itself)
+-- 2. Filtered on whether the Survey is considered a Poll
+--===================================================================================================
+
 SELECT DISTINCT R.Created Timestamp, S.ApplicationId, GlobalUserId, R.UserId, Q.SurveyId, Questions
 INTO ReportingDB.dbo.FactSurveys
 FROM Ratings.dbo.SurveyResponses R
