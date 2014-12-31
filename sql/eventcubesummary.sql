66f6b2ccb1517349ea690253ae2c61200801a098
Kevin Chiou <kevinchiou@DDs-MacBook-Pro-4.local>
2014-12-30 16:51:32 -0800

    added CSM Owner Name to EventCubeSummary

--- sql/eventcubesummary.sql
+++ sql/eventcubesummary.sql
@@ -8,7 +8,7 @@ IF OBJECT_ID('ReportingDB.dbo.EventCubeSummary','U') IS NOT NULL
 
 SELECT S.ApplicationId, Name, StartDate, EndDate,
 OpenEvent, LeadScanning, SurveysOn, InteractiveMap, Leaderboard, Bookmarking, Photofeed, AttendeesList, QRCode, ExhibitorReqInfo, ExhibitorMsg, PrivateMsging, PeopleMatching, SocialNetworks, RatingsOn,
-EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator, 
+EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator, OwnerName,
 BinaryVersion,
 ISNULL(Registrants,0) Registrants, ISNULL(Downloads,0) Downloads, Users, UsersActive, UsersFacebook, UsersTwitter, UsersLinkedIn, Sessions, Posts, PostsImage, PostsItem, Likes, Comments, Bookmarks, Follows, CheckIns, CheckInsHeadcount, Ratings, Reviews, Surveys,
 ISNULL(PromotedPosts,0) PromotedPosts, ISNULL(GlobalPushNotifications,0) GlobalPushNotifications
@@ -16,7 +16,7 @@ INTO ReportingDB.dbo.EventCubeSummary
 FROM
 ( SELECT S.ApplicationId, Name, StartDate, EndDate,
   OpenEvent, LeadScanning, SurveysOn, InteractiveMap, Leaderboard, Bookmarking, Photofeed, AttendeesList, QRCode, ExhibitorReqInfo, ExhibitorMsg, PrivateMsging, PeopleMatching, SocialNetworks, RatingsOn,
-  EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator,
+  EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator, OwnerName,
   B.BinaryVersion,
   COUNT(*) Users, SUM(Active) UsersActive, SUM(Facebook) UsersFacebook, SUM(Twitter) UsersTwitter, SUM(LinkedIn) UsersLinkedIn,
   SUM(Sessions) Sessions, SUM(Posts) Posts, SUM(PostsImage) PostsImage, SUM(PostsItem) PostsItem, SUM(Likes) Likes, SUM(Comments) Comments, SUM(Bookmarks) Bookmarks, SUM(Follows) Follows, SUM(CheckIns) CheckIns, SUM(CheckInsHeadcount) CheckInsHeadcount, SUM(Ratings) Ratings, SUM(Reviews) Reviews, SUM(Surveys) Surveys
@@ -24,7 +24,7 @@ FROM
   JOIN ReportingDB.dbo.DimEventBinaryVersion B ON S.ApplicationId = B.Applicationid
   GROUP BY S.ApplicationId, Name, StartDate, EndDate,
   OpenEvent, LeadScanning, SurveysOn, InteractiveMap, Leaderboard, Bookmarking, Photofeed, AttendeesList, QRCode, ExhibitorReqInfo, ExhibitorMsg, PrivateMsging, PeopleMatching, SocialNetworks, RatingsOn,
-  EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator, B.BinaryVersion
+  EventType, EventSize, AccountCustomerDomain, ServiceTierName, App365Indicator, OwnerName, B.BinaryVersion
 ) S
 LEFT OUTER JOIN
 ( SELECT ApplicationId, COUNT(DISTINCT UserId) Registrants
